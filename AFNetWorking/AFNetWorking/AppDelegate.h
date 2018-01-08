89f0e394b101c32a95eb8b556ee4c1c7e2723cba
贾辰 <951311987@qq.com>
2018-01-08 22:41:44 +0800

    Compay-get

---
+++ AFNetWorking/AFNetWorking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNetWorking
+//
+//  Created by 贾辰 on 18/1/8.
+//  Copyright © 2018年 贾辰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFNetWorking/AFNetWorking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetWorking
+//
+//  Created by 贾辰 on 18/1/8.
+//  Copyright © 2018年 贾辰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
