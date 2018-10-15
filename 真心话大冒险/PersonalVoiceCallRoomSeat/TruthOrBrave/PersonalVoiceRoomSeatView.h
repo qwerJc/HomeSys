385bf8ca41a2e80b2d433312ad005c422562f6bb
JiaChen <PX-jiachen@163.com>
2018-10-15 20:18:00 +0800

    no message

---
+++ 真心话大冒险/PersonalVoiceCallRoomSeat/TruthOrBrave/PersonalVoiceRoomSeatView.h
@@ -0,0 +1,13 @@
+//
+//  PersonalVoiceRoomSeatView.h
+//  LiveRoom
+//
+//  Created by 贾辰 on 2018/10/7.
+//  Copyright © 2018年 贾辰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PersonalVoiceRoomSeatView : UIView
+
+@end
